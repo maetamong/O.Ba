7c7b31745dd6881ff2ad5a3c72fa3ec1b7fff9f9
maetamong <daserism@gmail.com>
2016-08-16 10:36:45 +0900

    migration photos page

---
+++ app/controllers/bananas_controller.rb
@@ -0,0 +1,14 @@
+class BananasController < ApplicationController
+  def photos
+		@photos = Photo.all
+  end
+
+  def photos_user
+  end
+
+  def data
+  end
+
+  def points
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'bananas/photos'
+	get 'bananas/photos_user'
+  get 'bananas/data'
+  get 'bananas/points'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
